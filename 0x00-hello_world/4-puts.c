473507a239282d3929e0105100c92325386ee1e3
Michael Maina <pantalemonmongah@gmail.com>
2023-10-05 14:04:32 -0700

    Update desc for main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* main() - main block where program execution bigins */
+/**
+ *  main - main block where program execution bigins
+ * 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

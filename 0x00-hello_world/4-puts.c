a047f65f9c86ed63231612a8ac50124446e49723
Michael Maina <pantalemonmongah@gmail.com>
2023-10-05 13:54:41 -0700

    Update desc for main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /*
- * main - main block
+ * main() - main block where program execution bigins
  * Return: 0
  */
-/* The main function */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+/* The main() function */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

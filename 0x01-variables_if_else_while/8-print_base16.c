2ad49b6f496f1994adceaf02a2183749e4172719
Michael Maina <pantalemonmongah@gmail.com>
2023-10-06 00:17:05 -0700

    Add file 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - start of program execution
+ * Description: prits all base 16 numbers in lowercase
+ * Use putchar only, and three time only
+ */
+int main(void)
+{
+	char a;
+	int j;
+
+	for (j = 0; j < 10; j++){
+		putchar(j + '0');
+	}
+	for (a = 'a'; a < 'g'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}

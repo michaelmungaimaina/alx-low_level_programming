547f14e97315c8a330e35d7aa020c3d913172ba7
Michael Maina <pantalemonmongah@gmail.com>
2023-10-06 05:54:26 -0700

    Fix: Betty style

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,15 @@
  * main - start of program execution
  * Description: prits all base 16 numbers in lowercase
  * Use putchar only, and three time only
+ * Return: 0
  */
 int main(void)
 {
 	char a;
 	int j;
 
-	for (j = 0; j < 10; j++){
+	for (j = 0; j < 10; j++)
+	{
 		putchar(j + '0');
 	}
 	for (a = 'a'; a < 'g'; a++)

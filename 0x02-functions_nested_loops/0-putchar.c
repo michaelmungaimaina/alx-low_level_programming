579912ff9d92158b5268f5810bd7fb3015eecc69
Michael Maina <pantalemonmongah@gmail.com>
2023-10-11 03:21:03 -0700

    Add file main.h: update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - begin of program execution
  * Description: Prints '_putchar' followed
@@ -7,5 +7,15 @@
  */
 int main(void)
 {
-	printf("_putchar"\n");
+	char outPut[] = "_putchar";
+	int i = 0;
+
+	while (outPut[i] != '\0')
+	{
+		_putchar(outPut[i]);
+		i++
+	}
+	_putchar('\n');
+
+	return (0);
 }

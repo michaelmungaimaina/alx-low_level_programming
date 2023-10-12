ee5f44e5986b2d1b078c721d8ddff6e0e9577f83
Michael Maina <pantalemonmongah@gmail.com>
2023-10-12 07:39:31 -0700

    Renove empty space on 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,19 @@ void more_numbers(void)
 {
 	int i, j;
 
-	 i = 0;
-	 while (i < 10)
-	 {
-		 j = 0;
-		 while (j < 15)
-		 {
-			 if (j > 9)
-				 _putchar(j / 10 + '0');
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
 
-			 _putchar(j % 10 + '0');
-			 j++;
-		 }
-		 i++;
-		 _putchar('\n');
-	 }
+			_putchar(j % 10 + '0');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
 }

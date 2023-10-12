6e0783a1cbd2daad8cc06c2e711cfc6fc8166454
Michael Maina <pantalemonmongah@gmail.com>
2023-10-12 02:05:06 -0700

    Add file 0-isupper.c, README.md and main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase chars
+ * @c: integer input
+ * Return: 1 if uppercase, 0 ow
+ */
+int _isuppercase(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+# Task 0
+ - A function that checks for uppercase character.
+
+    Prototype: int _isupper(int c);
+    Returns 1 if c is uppercase
+    Returns 0 otherwise
+# Task 1
+ - 

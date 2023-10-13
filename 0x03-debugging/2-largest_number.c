92bded0c9ff1fa519b137c1133f9f344ec925e01
Michael Maina <pantalemonmongah@gmail.com>
2023-10-13 06:12:53 -0700

    Add file 2-largest_number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * largest_number - a func to print the largest number from 3 inputs
+ * @a: first integer input
+ * @b: second integer input
+ * @c: third integer input
+ * Return: Largest integer
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}

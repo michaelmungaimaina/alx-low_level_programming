5f4873aff8e40860967a41adeb8dbc9da40c3f4a
Michael Maina <pantalemonmongah@gmail.com>
2023-10-18 12:49:24 -0700

    Fix: Wrong use of semi-colon

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int destLen = 0; i = 0;
+	int destLen = 0, i = 0;
 
 	for (; dest[destLen] != '\0'; destLen++)
 		;
 
 	for (; i < n && src[i] != '\0'; i++, destLen++)
 		dest[destLen] = src[i];
+
 	dest[destLen] = '\0';
 
 	return (dest);

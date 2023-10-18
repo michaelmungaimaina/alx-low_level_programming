53d4dcd7d045cc1f0aaa64d0922fe1fc9885fafe
Michael Maina <pantalemonmongah@gmail.com>
2023-10-18 08:36:05 -0700

    Add file 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int j = 0; /** tracker of current char **/
+	int i; /** loop counter **/
+	char trigChar[] = ",;.!?(){}\n\t\" "; /** trigger for apitalizatio **/
+
+	while (str[j])
+	{
+		for (i = 0; trigChar[i] != '\0'; i++)
+		{
+			if ((j == 0 || str[j - 1] == trigChar[i]) && (str[j] >= 97 && str[j] <= 122))
+				s[j] -= 32;
+		}
+		
+		j++;
+	}
+
+	return (str);
+}

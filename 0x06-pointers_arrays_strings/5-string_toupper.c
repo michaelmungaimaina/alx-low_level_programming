edeaec692f1ff9216266d52814cb9f94640bde8c
Michael Maina <pantalemonmongah@gmail.com>
2023-10-18 07:58:37 -0700

    Add file 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * strint_toupper - Changes all lowercase letters to uppercase
+ * @c: String
+ * Return: c
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] > 96 && c[i] < 123)
+			c[i] -= 32;
+	}
+	return (c);
+}

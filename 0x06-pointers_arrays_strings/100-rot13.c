1b0266de51268c2fae198132b92bc518d5295907
Michael Maina <pantalemonmongah@gmail.com>
2023-10-18 10:42:36 -0700

    Update file 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,31 +1,17 @@
 #include "main.h"
 
-/**
- * rot13 - Rotates a character by 13 positions in the alphabet
- * @s: string
- * Return: cipher string 's'
- */
-
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int i = 0;
-
-	while (s[i])
-	{
-		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
-			{
-				s[i] -= 13;
-				break;
-			}
-
-			s[i] += 13;
-			break;
-		}
-
-		i++;
-	}
-
-	return (s);
+	 char *s = str;
+	 while (*s)
+	 {
+		 char offset = (*s >= 'a') ? 'a' : 'A';
+		 if ((*s >= 'A' && *s <= 'Z') || (*s >= 'a' && *s <= 'z'))
+		 {
+			 *s = (((*s - offset + 13) % 26) + offset);
+		 }
+		 s++;
+	 }
+	 
+	 return (str);
 }

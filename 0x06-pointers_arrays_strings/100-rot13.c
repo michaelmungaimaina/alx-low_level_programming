68eee74d245c8c28339c17184864941853164020
Michael Maina <pantalemonmongah@gmail.com>
2023-10-19 02:32:24 -0700

    Fix: rot13 algorithm

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,17 +1,29 @@
 #include "main.h"
 
-char *rot13(char *str)
+/**
+ * rot13 - encodes string using rot13 cipher
+ * @s: string to be ciphered
+ * Return: pointer to dest
+ */
+
+char *rot13(char *s)
 {
-	 char *s = str;
-	 while (*s)
-	 {
-		 char offset = (*s >= 'a') ? 'a' : 'A';
-		 if ((*s >= 'A' && *s <= 'Z') || (*s >= 'a' && *s <= 'z'))
-		 {
-			 *s = (((*s - offset + 13) % 26) + offset);
-		 }
-		 s++;
-	 }
-	 
-	 return (str);
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
 }

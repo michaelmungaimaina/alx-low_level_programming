9e25dbb171033f89548aadbfdf8e88e5a5e096d2
maina michael <pantalemonmongah@gmail.com>
2023-10-25 23:50:59 +0300

    Add file 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+int _palindrome(char *s);
+
+/**
+ * is_palindrome - Returns if a string is palindrome
+ * @s: the string value to be checked
+ *
+ * Return: integer value
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '0')
+		return (1);
+
+	return (_palindrome(s));
+}
+
+/**
+ * _palindrome - Check if a string is palindrome
+ * @s: the string value to be checked
+ *
+ * Return: integer value
+ */
+int _palindrome(char *s)
+{
+	int l = _strlen(s) - 1;
+
+	if (*s == s[l])
+	{
+		s++;
+		l--;
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * _strlen - Get the length of a string
+ * @s: the string to get the length
+ *
+ * Return: the string length
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen(s) + 1);
+}

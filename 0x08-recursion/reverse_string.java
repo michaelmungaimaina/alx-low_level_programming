2dc454ce1dac37c308db474186f4d10b1f75252d
maina michael <pantalemonmongah@gmail.com>
2023-10-25 21:07:53 +0300

    Add 2-strlen_recursion.c - returns atring length

---
+++ 0x08-recursion/reverse_string.java
@@ -0,0 +1,10 @@
+void reverse(String str)
+{
+	if ((str==null)||(str.length() <= 1))
+		System.out.println(str);
+	else
+	{
+		System.out.print(str.charAt(str.length()-1));
+		reverse(str.substring(0,str.length()-1));
+	}
+}

e099a7c7473de529b381abf644872aaca99524f4
maina michael <pantalemonmongah@gmail.com>
2023-10-25 22:08:31 +0300

    Update file 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,25 +8,24 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
-
-/**
- * _sqrt - helper func to calculates natural square root
- * @n: number to calculate the square root
- * @i: iteration number
- *
- * Return: the natural square root
- */
-int _sqrt(int n, int i)
-{
-	int square = i * i;
-
-	if (square == n)
-		return (i);
-
-	if (square < n)
-		return (_sqrt(n, i + 1));
-
-	return (-1);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int root = _sqrt_recursion(n - 1);
+		if (root * root == n)
+		{
+			return (root);
+		}
+		else
+		{
+			return (_sqrt_recursion(n - 1));
+		}
+	}
 }

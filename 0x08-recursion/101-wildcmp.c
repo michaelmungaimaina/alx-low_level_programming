60fd875459b459779565223ca6a3cb23287dcaf2
maina michael <pantalemonmongah@gmail.com>
2023-10-26 00:55:31 +0300

    Update file 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,31 +25,41 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s1 == *s2)
 	{
-		return wildcmp(++s1, ++s2);
+		return (wildcmp(++s1, ++s2));
 	}
 	else if (*s1 == '*')
 	{
 		if (*(s1 + 1) == '*')
-			return wildcmp(++s1, s2);
+			return (wildcmp(++s1, s2));
 		else
 		{
-			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
+			return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2));
 		}
 	}
 	else if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-			return wildcmp(s1, ++s2);
+			return (wildcmp(s1, ++s2));
 		else
 		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
+			return (wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2));
 		}
 	}
 
 	return (0);
 }
 
-
+/**
+ * findscr - Finds source
+ * @s: string
+ * @c: string
+ * @i: integer
+ * @p: integer
+ *
+ * Description: Finds the source
+ *
+ * Return: integer
+ */
 int findsrc(char *s, char c, int i, int p)
 {
 	if (*(s + i) == '\0')

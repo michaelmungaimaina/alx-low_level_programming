1de5467c1c8f8acece7b0a8e88331dd704d252c2
maina michael <pantalemonmongah@gmail.com>
2024-01-15 16:53:10 +0300

    Add file 100-operations.c, 100-operations.so,

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,28 @@
+// 100-operations.c
+
+int add(int a, int b) {
+    return (a + b);
+}
+
+int sub(int a, int b) {
+    return (a - b);
+}
+
+int mul(int a, int b) {
+    return (a * b);
+}
+
+int div(int a, int b) {
+    if (b == 0) {
+        return (-1);  // Indicate division by zero
+    }
+    return (a / b);
+}
+
+int mod(int a, int b) {
+    if (b == 0) {
+        return (-1);  // Indicate modulo by zero
+    }
+    return (a % b);
+}
+
